114c8bb414f23a789facbf4249ba5f3fcc14f7c1
lu78abranches <luisabranches.violao@gmail.com>
2026-06-13 16:44:54 -0300

    feat(ch02): challenge-09 - Casting and Instanceof

--- ch02/challenge-07/com/ocp/model/Main.java
+++ ch02/challenge-07/com/ocp/model/Main.java
@@ -11,6 +11,17 @@ class Main{
 		for(Animal animals : animal){
 			animals.sound();
 		}
+		Animal a = new Dog();  // upcasting implícito
+		Dog d = (Dog) a;       // downcasting explícito — compiles because obj a belongs to subclass Dog
+		//Cat c = (Cat) a;       // compila? roda? not compiles, object a not belongs to subclass Cat, throw exception in runtime java.lang.ClassCastException
+
+		if(a instanceof Dog) {
+    			System.out.println("A Dog is a Animal");
+		}
+		if(a instanceof Dog d2) {
+    			d2.sound();
+		}
+		
 	}
 }
 
