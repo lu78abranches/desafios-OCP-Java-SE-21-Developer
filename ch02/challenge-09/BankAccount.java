218f5f9a98ca976d6a1819d99d6f854404a1b451
lu78abranches <luisabranches.violao@gmail.com>
2026-06-13 17:58:21 -0300

    fix: challenge-09

--- ch02/challenge-09/BankAccount.java
+++ ch02/challenge-09/BankAccount.java
@@ -2,13 +2,17 @@ public class BankAccount{
 	private double balance;
 	private String owner;
 
+	public BankAccount(String Owner){
+	 this.owner = owner;
+	}
+
 	public double getBalance(){
 	  return balance;
 	}
 	public String getOwner(){
 	  return owner;
 	}
-	public void setBalance(){
+	public void setBalance(Double balance){
 	 if(balance >= 0){	
 		this.balance = balance;
 	} else {
